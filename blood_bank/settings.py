530f5f151b2245bd34745b3c175bea75df5ef6bc
sakayat <sakhyatkabir1@gmail.com>
2024-07-16 03:00:09 +0600

    updated

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -134,6 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CSRF_TRUSTED_ORIGINS = True
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
