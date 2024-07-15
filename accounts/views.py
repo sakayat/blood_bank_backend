ae558553ef05dbcf831de62b398415cd0bf1537b
sakayat <sakhyatkabir1@gmail.com>
2024-07-16 03:26:30 +0600

    updated

--- accounts/views.py
+++ accounts/views.py
@@ -22,7 +22,7 @@ class UserRegistration(APIView):
             user = serializer.save()
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link = f"http://127.0.0.1:8000/api/accounts/active/{uid}/{token}"
+            confirm_link = f"https://humane-donor.netlify.app/api/accounts/active/{uid}/{token}"
             email_subject = "Confirm Your Email"
             email_body = render_to_string(
                 "confirm_mail.html", {"confirm_link": confirm_link}
