cf9165bde81df820acb22764fcb82fd280c4bc15
sakayat <sakhyatkabir1@gmail.com>
2024-07-29 15:57:25 +0600

    updated

--- accounts/views.py
+++ accounts/views.py
@@ -24,7 +24,7 @@ class UserRegistration(APIView):
             user = serializer.save()
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link = f"{os.getenv("API_URL")}/api/accounts/active/{uid}/{token}"
+            confirm_link = f"{os.getenv("BASE_API_URL")}/api/accounts/active/{uid}/{token}"
             email_subject = "Confirm Your Email"
             email_body = render_to_string(
                 "confirm_mail.html", {"confirm_link": confirm_link}
