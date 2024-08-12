d40a4969e42905cee2d7138d1246dc7d77145a9b
sakayat <sakhyatkabir1@gmail.com>
2024-08-13 02:50:18 +0600

    updated

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -137,7 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CSRF_TRUSTED_ORIGINS = ["https://blood-bank-backend-1sf7.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ["https://humane-donor.netlify.app"]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
